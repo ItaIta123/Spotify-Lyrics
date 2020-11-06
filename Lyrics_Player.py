5e4f9e81f2e5f8eca39ec9d6c841fd005bae7c41
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2020-11-06 11:33:12 +0200

    Update Lyrics_Player.py

--- Lyrics_Player.py
+++ Lyrics_Player.py
@@ -60,30 +60,5 @@ def main():
 
 
 
-# get_songs_list_lyrics(['https://genius.com/Eminem-rap-god-lyrics', 'https://genius.com/The-scotts-the-scotts-lyrics',
-# 'https://genius.com/Drake-gods-plan-lyrics',
-# 'https://genius.com/Lil-uzi-vert-xo-tour-llif3-lyrics',
-# 'https://genius.com/Idina-menzel-let-it-go-lyrics'])
-
-# -------------------------------------------------------------------------------------------------------|
-# Some Basic Explanation:                                                                                |
-# source = requests.get().text  # get the source code of the website as text                             |
-# # print(source) = prints the exact HTML as it seems on the web (with inspect) .                        |
-# soup = BeautifulSoup(source, 'lxml')  # Turn the website's HTML source code to more convenient,        |
-# readable HTML text.                                                                                    |
-# # print(soup.prettify())  # prettify makes the HTML text more structured.                              |
-# -------------------------------------------------------------------------------------------------------|
-
-# -------------------------------------------------------------------------------------------------------|
-# The First Working Sequence Of Actions To Make It Work:                                                 |
-# source = requests.get('https://genius.com/Lil-uzi-vert-xo-tour-llif3-lyrics').text                     |
-# soup = BeautifulSoup(source, 'lxml')                                                                   |
-# lyrics_page_section = soup.find("div", class_="lyrics")                                                |
-# print()                                                                                                |
-# lyrics = lyrics_page_section.p.text                                                                    |
-# print(lyrics)                                                                                          |
-# -------------------------------------------------------------------------------------------------------|
-
-
 if __name__ == '__main__':
     main()
